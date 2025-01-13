b36acde634e37bbbf9dfa9bb8b7bdf48163deeb9
Eduardo Bif <eduardobif@hotmail.com>
2025-01-13 20:37:55 -0300

    - Added feature to save and load blockchain from json file. - Added nonce e difficulty parametes to the block. - Added repair function for invalid blocks.

--- blockchain.go
+++ blockchain.go
@@ -3,7 +3,10 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
+	"io"
+	"os"
 	"time"
 )
 var blockchain []Block
@@ -14,10 +17,12 @@ type Block struct {
 	Data string 
 	PrevHash string
 	Hash string
+	Nonce int
+	Difficulty int
 }
 
 func calculateHash(block Block) string{
-	record := fmt.Sprintf("%d%s%s%s", block.Index,  block.Timestamp, block.Data, block.PrevHash)
+	record := fmt.Sprintf("%d%s%s%s%d", block.Index,  block.Timestamp, block.Data, block.PrevHash, block.Nonce)
 	hash := sha256.Sum256([]byte(record))
 
 	return hex.EncodeToString(hash[:])
@@ -29,6 +34,8 @@ func createGenesisBlock() Block{
 		Timestamp: time.Now().String(),
 		Data: "Bloco Gênesis",
 		PrevHash: "0",
+		Nonce: 0,
+		Difficulty: 0,
 	}
 
 	genesisBlock.Hash = calculateHash(genesisBlock)
@@ -37,14 +44,22 @@ func createGenesisBlock() Block{
 	return genesisBlock
 }
 
-func addBlock(data string) Block{
+func addBlock(data string, difficulty int) Block{
+	
+	if difficulty > 5 {
+		fmt.Println("Dificuldade muito alta, ajustando para 5.")
+		difficulty = 5
+	}
+	
 	newBlock := Block{
 		Index : (blockchain[len(blockchain)-1].Index + 1),
 		Timestamp: time.Now().String(),
 		Data: data,
 		PrevHash: blockchain[len(blockchain)-1].Hash,
+		Nonce: 0,
+		Difficulty: difficulty,
 	}
-	newBlock.Hash = calculateHash(newBlock)
+	runProofOfWork(&newBlock)
 	blockchain = append(blockchain, newBlock)
 
 	return newBlock
@@ -54,23 +69,99 @@ func isBlockchainValid() bool {
 	for i := 1; i < len(blockchain); i++{
 		currentBlock := blockchain[i]
 		prevBlock := blockchain[i - 1]
-		if currentBlock.PrevHash != prevBlock.Hash {return false}
-		if calculateHash(currentBlock) != currentBlock.Hash {return false}
+		if currentBlock.PrevHash != prevBlock.Hash || calculateHash(currentBlock) != currentBlock.Hash {return false}
 	}
 	return true
 }
-func main(){
-	createGenesisBlock()
 
-    // Adicionando novos blocos
-    addBlock("Bloco 1: Transação A -> B")
-    addBlock("Bloco 2: Transação C -> D")
-    addBlock("Bloco 3: Transação E -> F")
+func saveBlockchainToFile(){
+	file, err := os.Create("data.json") // Tenta criar o arquivo
+	if err != nil {
+		fmt.Println("Erro ao criar o arquivo:", err)
+		return // Encerra a função em caso de erro
+	}
+	defer file.Close()
+	jsonData, err := json.MarshalIndent(blockchain, "", "  ")
+	if err != nil {
+		fmt.Println("Erro ao converter para JSON:", err)
+		return
+	}
+	_, err = file.Write(jsonData)
+	if err != nil {
+		fmt.Println("Erro ao escrever no arquivo:", err)
+		return
+	}
+	fmt.Println("Blockchain salva com sucesso em data.json")
+}
 
-    // Imprimindo todos os blocos na blockchain
-    for i, block := range blockchain {
-        fmt.Printf("Bloco %d: %+v\n", i, block)
+func repairBlockchain(){
+	originalLength := len(blockchain)
+	for i := 1; i < len(blockchain); i++{
+		currentBlock := blockchain[i]
+		prevBlock := blockchain[i - 1]
+		if currentBlock.PrevHash != prevBlock.Hash || calculateHash(currentBlock) != currentBlock.Hash {
+			fmt.Printf("Bloco %d é inválido. Removendo blocos subsequentes...\n", currentBlock.Index)
+			blockchain = blockchain[:i] // Remove todos os blocos após o inválido
+			fmt.Printf("Blockchain reparada. %d blocos removidos.\n", originalLength-i)
+			saveBlockchainToFile()     // Salva a blockchain corrigida
+			return
+		}
+	}
+	fmt.Println("Blockchain está válida.")
+}
+
+func loadBlockchainFromFile(){
+	file, err := os.Open("data.json")
+	if err != nil {
+		fmt.Println("Arquivo não encontrado. Criando nova blockchain com bloco gênesis...")
+		createGenesisBlock()
+		return
+	}
+	defer file.Close()
+	blockchainData, err := io.ReadAll(file)
+	if err != nil {
+		fmt.Println("Erro ao ler o arquivo:", err)
+		return
+	}
+	err = json.Unmarshal(blockchainData, &blockchain)
+	if err != nil {
+		fmt.Println("Erro ao desserializar JSON:", err)
+		return
+	}
+	fmt.Println("Blockchain carregada com sucesso!")
+	if isBlockchainValid() {
+        fmt.Println("Blockchain é válida.")
+    } else {
+        fmt.Println("Erro: Blockchain carregada é inválida.")
+		repairBlockchain()
     }
-	fmt.Println("Blockchain é válida?", isBlockchainValid())
+}
 
+func runProofOfWork(block *Block){
+	prefix := ""
+    for i := 0; i < block.Difficulty; i++ {
+        prefix += "0"
+    }
+	start := time.Now()
+	for{
+		block.Hash = calculateHash(*block)
+		if block.Hash[:block.Difficulty] == prefix{
+			fmt.Printf("Bloco minerado em %s! Nonce: %d, Hash: %s\n", time.Since(start), block.Nonce, block.Hash)
+            break	
+		}
+		block.Nonce++
+		if block.Nonce%10000 == 0 { // Log a cada 10.000 tentativas
+            fmt.Printf("Tentativa atual: Nonce=%d, Hash=%s\n", block.Nonce, block.Hash)
+        }
+	}
+}
+
+
+func main(){
+	loadBlockchainFromFile()
+
+	addBlock("aaa", 2)
+	addBlock("bbb", 3)
+ 
+    saveBlockchainToFile()
 }
